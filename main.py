48c2344a403a2123cef077b2f991552ff2125f28
hikingout <hikingout@msn.com>
2023-12-03 18:21:02 +0000

    Day 3 submission

--- main.py
+++ main.py
@@ -2,4 +2,5 @@
 #from answers import aoc23_12_01_P1
 #from answers import aoc23_12_01_P2
 #from answers import aoc23_12_02_P1
-from answers import aoc23_12_02_P2
+#from answers import aoc23_12_03_P1
+from answers import aoc23_12_03_P2
