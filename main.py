0b8bddb3858ea1d5add9b771245a43993285f197
hikingout <hikingout@msn.com>
2023-12-04 17:33:39 +0000

    gitignore update

--- main.py
+++
@@ -1,7 +0,0 @@
-#Change to file name for current challenge
-#from answers import aoc23_12_01_P1
-#from answers import aoc23_12_01_P2
-#from answers import aoc23_12_02_P1
-#from answers import aoc23_12_03_P1
-#from answers import aoc23_12_03_P2
-from answers import aoc23_12_04_P1
